de367b8694b6e63777bc0f5280a375e26be2ee32
Marco Novaes <mako1tube2@gmail.com>
2021-09-02 21:23:25 -0300

    Primeiro commit

---
+++ src/pages/HomePage.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+const HomePage = () => (
+    <>
+<h1>Hello, Welcome to my blog!</h1>
+
+<p>
+    This is my personal blog, I will post many content about web development.
+    How to buildo websites with the most modern techonology like Reactjs, Nextjs, Bootstrap.
+
+</p>
+
+<p>
+    I will sometimens translate the content I see on the internet.
+    Tutorial for all developers in all levels. To help the community to be more and more stronger.
+</p>
+
+    </>
+        
+)
+
+export default HomePage;
