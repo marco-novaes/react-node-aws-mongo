30a3f669172065b2ad0703f1e139d83e54a5ff9a
Marco Novaes <mako1tube2@gmail.com>
2021-09-02 21:53:48 -0300

    Segundo commit

--- src/pages/ArticlePage.js
+++ src/pages/ArticlePage.js
@@ -1,11 +1,15 @@
 import React from 'react';
+import ArticleList from '../components/ArticlesList';
+import NotFoundPage from './NotFoundPage';
 import articleContent from './article-content';
 
 const ArticlePage = ({ match }) => { 
     const name = match.params.name;
     const article = articleContent.find(article => article.name === name );
 
-if(!article) return <h1>Article does not exist!</h1>
+    if(!article) return <NotFoundPage />
+
+    const otherArticles = articleContent.filter(article => article.name !== name);
 
     return (
     <>
@@ -13,6 +17,8 @@ if(!article) return <h1>Article does not exist!</h1>
 {article.content.map((paragraph, key) => (
     <p key={key}>{paragraph}</p>
 ))}
+    <h3>Other Articles</h3>
+    <ArticleList articles={otherArticles} />
     </>
         
 );
